b4875e62b7e78ab824e3abf4b99b63654a287d26
David Heras <davidherasp@gmail.com>
2018-03-10 14:16:27 +0100

    Project update in progress to support more leagues

--- slds/slds.py
+++ slds/slds.py
@@ -2,7 +2,8 @@ from riotwatcher import RiotWatcher
 from converters.data2frames import game_to_dataframe as g2df
 from converters.data2files import write_json, read_json, save_runes_reforged_json
 from config.constants import SLO_MATCHES_FILE_PATH, SLO_DATA_DTYPES, SLO_MATCHES_DATA_P_COLS, SLO_CUSTOM_POSITIONS, \
-    API_KEY, SLO_GAMES_DIR, SLO_DATASETS_DIR, SLO_DATASET_CSV, STATIC_DATA_DIR
+    API_KEY, SLO_GAMES_DIR, EXPORTS_DIR, SLO_DATASET_CSV, STATIC_DATA_DIR, SUPPORTED_LEAGUES, LEAGUES_DATA_DICT, \
+    LCK_DATA_DTYPES
 import pandas as pd
 from datetime import datetime as dt
 from tqdm import tqdm
@@ -11,15 +12,16 @@ import argparse
 
 
 class Slds:
-    def __init__(self, region):
+    def __init__(self, region, league):
         self.rw = RiotWatcher(API_KEY)
         self.region = region
+        self.league = league
 
     def generate_dataset(self, read_dir, force_update=False):
         df = pd.read_csv(SLO_MATCHES_FILE_PATH, dtype=SLO_DATA_DTYPES)
         new = list(df.game_id.unique())
         try:
-            df2 = pd.read_csv('{}{}'.format(SLO_DATASETS_DIR, SLO_DATASET_CSV), index_col=0)
+            df2 = pd.read_csv('{}{}'.format(EXPORTS_DIR, SLO_DATASET_CSV), index_col=0)
             old = list(df2.gameId.unique())
             new_ids = self.__get_new_ids(old, new)
         except FileNotFoundError:
@@ -72,9 +74,9 @@ class Slds:
             raise TypeError('Dict expected at data param. Should be passed as shown here: {"match": match_dict, '
                             '"timeline": timeline_dict}.')
 
-    @staticmethod
-    def get_slo_games_ids():
-        df = pd.read_csv(SLO_MATCHES_FILE_PATH, dtype=SLO_DATA_DTYPES)
+    def get_league_game_ids(self):
+        league_data_path = LEAGUES_DATA_DICT[self.league]['matches_file_path']
+        df = pd.read_csv(league_data_path, dtype=LCK_DATA_DTYPES)
         return list(df.game_id)
 
     @staticmethod
@@ -89,9 +91,9 @@ class Slds:
 
     def __concat_games(self, df, read_dir):
         return pd.concat([g2df(match=read_json(save_dir=read_dir,
-                                                    file_name=self.__get_file_names_from_match_id(m_id=g[1]['game_id'],
-                                                                                                  save_dir=read_dir)
-                                                    ['match_filename']),
+                                               file_name=self.__get_file_names_from_match_id(m_id=g[1]['game_id'],
+                                                                                             save_dir=read_dir)
+                                               ['match_filename']),
                                timeline=read_json(save_dir=read_dir,
                                                   file_name=self.__get_file_names_from_match_id(
                                                       m_id=g[1]['game_id'], save_dir=read_dir)['tl_filename']),
@@ -113,7 +115,9 @@ class Slds:
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='LoL solution to transform match data to DataFrames and CSV firstly used for SLO.')
+    parser = argparse.ArgumentParser(description='LoL solution to transform match data to DataFrames and CSV firstly '
+                                                 'used for SLO.')
+    parser.add_argument('-l', '--league', help='Choose league. [LCK, SLO]')
     parser.add_argument('-ex', '--export', help='Export dataset.', action='store_true')
     parser.add_argument('-X', '--xlsx', help='Export dataset as XLSX.', action='store_true')
     parser.add_argument('-C', '--csv', help='Export dataset as CSV.', action='store_true')
@@ -124,11 +128,19 @@ def parse_args():
 
 
 def main():
-    slds = Slds(region='EUW1')
     args = parse_args()
+    league = args.league.upper()
+    if league not in SUPPORTED_LEAGUES:
+        print('League {} is not currently supported. Check help for more information.'.format(args.league))
+        return
+    
+    slds = Slds(region='EUW1', league=league)
     if args.download:
-        ids = slds.get_slo_games_ids()
-        slds.download_games(ids=ids, save_dir=SLO_GAMES_DIR)
+        if LEAGUES_DATA_DICT[league]['official']:
+
+        else:
+            ids = slds.get_league_game_ids()
+            slds.download_games(ids=ids, save_dir=LEAGUES_DATA_DICT[league]['games_path'])
         print("Games downloaded.")
     
     if args.update_static_data:
@@ -139,12 +151,12 @@ def main():
         df = slds.generate_dataset(read_dir=SLO_GAMES_DIR, force_update=args.force_update)
         if df is not None:
             if args.xlsx:
-                df.to_excel('{}slo_dataset.xlsx'.format(SLO_DATASETS_DIR))
+                df.to_excel('{}slo_dataset.xlsx'.format(EXPORTS_DIR))
             if args.csv:
-                df.to_csv('{}slo_dataset.csv'.format(SLO_DATASETS_DIR))
+                df.to_csv('{}slo_dataset.csv'.format(EXPORTS_DIR))
             if not args.csv and not args.xlsx:
-                df.to_excel('{}slo_dataset.xlsx'.format(SLO_DATASETS_DIR))
-                df.to_csv('{}slo_dataset.csv'.format(SLO_DATASETS_DIR))
+                df.to_excel('{}slo_dataset.xlsx'.format(EXPORTS_DIR))
+                df.to_csv('{}slo_dataset.csv'.format(EXPORTS_DIR))
             print("Export finished.")
         else:
             print("No export done.")
